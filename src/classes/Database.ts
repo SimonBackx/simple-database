e614fb4f432f32ff9ac7bb81a474122ad749deb5
Simon Backx <hi@simonbackx.com>
2020-09-07 14:12:07 +0200

    Added support for port

--- src/classes/Database.ts
+++ src/classes/Database.ts
@@ -10,6 +10,7 @@ const pool = mysql.createPool({
     host: process.env.DB_HOST ?? "localhost",
     user: process.env.DB_USER ?? "root",
     password: process.env.DB_PASS ?? "root",
+    port: parseInt(process.env.DB_PORT ?? "3306"),
     database: process.env.DB_DATABASE,
     waitForConnections: true,
     connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT ?? "10"),
