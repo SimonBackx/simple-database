4778c7933288d9ce484e543923c2584a6c5b4df6
Simon Backx <hi@simonbackx.com>
2020-10-26 08:42:19 +0100

    Loader for many to one relation

--- src/classes/ManyToOneRelation.ts
+++ src/classes/ManyToOneRelation.ts
@@ -1,3 +1,4 @@
+import { Database } from './Database';
 import { Model } from "./Model";
 
 export class ManyToOneRelation<Key extends keyof any, M extends Model> {
@@ -31,5 +32,19 @@ export class ManyToOneRelation<Key extends keyof any, M extends Model> {
     /// Generate a join query
     joinQuery(namespaceA: string, namespaceB: string): string {
         return `LEFT JOIN ${this.model.table} as ${namespaceB} on ${namespaceB}.${this.model.primary.name} = ${namespaceA}.${this.foreignKey}\n`;
+    }
+
+    /// Load the relation of a list of models
+    async load(modelsA: Model[]) {
+        let str = `SELECT ${this.model.getDefaultSelect()} FROM ${this.model.table}\n`;
+        str += `WHERE ${this.model.primary.name} IN (?)`;
+       
+        const [rows] = await Database.select(str, [modelsA.map(m => m[this.foreignKey])]);
+        const modelsB = this.model.fromRows(rows, this.model.table) as M[];
+       
+        for (const model of modelsA) {
+            model.setOptionalRelation(this, modelsB.find(m => m.getPrimaryKey() === model[this.foreignKey]) ?? null)
+        }
+        return modelsB;
     }
 }
