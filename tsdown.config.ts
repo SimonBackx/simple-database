13b08224036726baec25067bdffb04b0478eba0e
Simon Backx <simon@stamhoofd.be>
2026-03-19 15:10:18 +0100

    Added missing streets data

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsdown';
 
 export default defineConfig({
-    entry: 'index.ts',
+    entry: ['index.ts', 'src/classes/data/*.ts'],
     format: ['cjs', 'esm'],
     unbundle: true,
 });
