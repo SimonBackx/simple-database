d7ede13bef63e0e91cc8273854aee8ab60d7b742
Simon Backx <simon@stamhoofd.be>
2026-03-20 08:26:52 +0100

    Added __dirname shims in ESM output

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,5 +3,12 @@ import { defineConfig } from 'tsdown';
 export default defineConfig({
     entry: ['index.ts', 'src/classes/data/*.ts'],
     format: ['cjs', 'esm'],
-    unbundle: true,
+    unbundle: false,  // SHIMS IS NOT COMPATIBLE WITH UNBUNDLE:TRUE!
+    dts: true,
+    sourcemap: true,
+    shims: true, // SHIMS IS NOT COMPATIBLE WITH UNBUNDLE:TRUE!
+    tsconfig: './tsconfig.build.json',
+    outExtensions: () => ({
+        dts: '.d.ts',
+    }),
 });
